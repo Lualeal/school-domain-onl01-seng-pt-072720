c4cdef0e29e31fc1f31e76d13545790c6bc44d30
Luana Leal <luana.leal.eq@gmail.com>
2020-08-13 02:59:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,3 +18,24 @@ class School
   roster["new_key"] << "new_value_for_value_array"
   
 end 
+
+
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
+  end
+  
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
+
+end
