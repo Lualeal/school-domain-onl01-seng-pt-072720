915a94f4499cdf4abc30fcd608f6bcb78575096e
Luana Leal <luana.leal.eq@gmail.com>
2020-08-13 02:58:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,6 +10,10 @@ class School
   def add_student(students_name, grade)
   end 
   
+  
+  def grade(grade_arg)
+  end 
+  
   roster["new_key"] = []
   roster["new_key"] << "new_value_for_value_array"
   
